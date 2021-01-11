43057e1081cb6b1a46954bf2a3c80adbcdf00576
Alexey Chuprin <a.chuprin@capjack.ru>
2021-01-11 14:08:07 +0300

    JVM 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "1.4.20"
-	id("nebula.release") version "15.3.0"
+	kotlin("jvm") version "1.4.21"
+	id("nebula.release") version "15.3.1"
 	id("ru.capjack.bintray") version "1.0.0"
 }
 
@@ -14,17 +14,17 @@ repositories {
 
 kotlin {
 	target {
-		compilations.all { kotlinOptions.jvmTarget = "1.8" }
+		compilations.all { kotlinOptions.jvmTarget = "11" }
 	}
 }
 
 dependencies {
 	implementation(kotlin("reflect"))
 	implementation("ch.qos.logback:logback-classic:1.2.3")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
-	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.3")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+	implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
 	
-	api("ru.capjack.tool:tool-utils:1.1.1")
+	api("ru.capjack.tool:tool-utils:1.2.0")
 	api("ru.capjack.tool:tool-logging:1.2.2")
 	api("ru.capjack.tool:tool-depin:0.9.0")
 }
